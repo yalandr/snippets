35b6eafe5a693c3a362983aee577a5b2ad3ca722
yalandr <52127093+yalandr@users.noreply.github.com>
2022-05-19 20:28:48 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
-// Accordion
-var acc = document.getElementsByClassName("item-name");
-var i;
+document.addEventListener("DOMContentLoaded", () => {
+    // Accordion
+    var acc = document.getElementsByClassName("item-name");
+    var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
     }
-  });
-}
+});
+
