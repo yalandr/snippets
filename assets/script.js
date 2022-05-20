10ba372ee141c2b089ecb67f8f57d0578c50cd82
yalandr <52127093+yalandr@users.noreply.github.com>
2022-05-20 09:16:55 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -14,5 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
     }
+
+    // FILTER
+    const itemCards = document.querySelectorAll('.item-card');
+
+		document.querySelector('nav').addEventListener('click', event => {
+			if (event.target.tagName !== 'LI') return false;
+			let filterClass = event.target.dataset['filter'];
+			itemCards.forEach( elem => {
+				elem.classList.remove('hide');
+				if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+					elem.classList.add('hide');
+				}
+			})
+
+		})
 });
 
